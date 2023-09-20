f5e27ea3c1609940971a0af698f8450f9d041e60
sunhang1998 <61487272+sunhang1998@users.noreply.github.com>
2023-09-20 20:05:02 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,272 @@
+import os
+import sys
+import json
+import pickle
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+import csv
+import matplotlib.pyplot as plt
+import math
+
+def caps_loss(y_true, y_pred, x, x_recon, lam_recon):
+    """
+    Capsule loss = Margin loss + lam_recon * reconstruction loss.
+    :param y_true: true labels, one-hot coding, size=[batch, classes]
+    :param y_pred: predicted labels by CapsNet, size=[batch, classes]
+    :param x: input data, size=[batch, channels, width, height]
+    :param x_recon: reconstructed data, size is same as `x`
+    :param lam_recon: coefficient for reconstruction loss
+    :return: Variable contains a scalar loss value.
+    """
+    L = y_true * torch.clamp(0.9 - y_pred, min=0.) ** 2 + \
+        0.5 * (1 - y_true) * torch.clamp(y_pred - 0.1, min=0.) ** 2
+    L_margin = L.sum(dim=1).mean()
+
+    L_recon = nn.MSELoss()(x_recon, x)
+    
+    return L_margin + lam_recon * L_recon
+def read_split_data(root: str, val_rate: float = 0.2):
+    random.seed(0)  # 保证随机结果可复现
+    assert os.path.exists(root), "dataset root: {} does not exist.".format(root)
+
+    # 遍历文件夹，一个文件夹对应一个类别
+    flower_class = [cla for cla in os.listdir(root) if os.path.isdir(os.path.join(root, cla))]
+    # 排序，保证顺序一致
+    flower_class.sort()
+    # 生成类别名称以及对应的数字索引
+    class_indices = dict((k, v) for v, k in enumerate(flower_class))
+    json_str = json.dumps(dict((val, key) for key, val in class_indices.items()), indent=4)
+    with open('class_indices.json', 'w') as json_file:
+        json_file.write(json_str)
+
+    train_images_path = []  # 存储训练集的所有图片路径
+    train_images_label = []  # 存储训练集图片对应索引信息
+    val_images_path = []  # 存储验证集的所有图片路径
+    val_images_label = []  # 存储验证集图片对应索引信息
+    every_class_num = []  # 存储每个类别的样本总数
+    supported = [".jpg", ".JPG", ".png", ".PNG"]  # 支持的文件后缀类型
+    # 遍历每个文件夹下的文件
+    for cla in flower_class:
+        cla_path = os.path.join(root, cla)
+        # 遍历获取supported支持的所有文件路径
+        images = [os.path.join(root, cla, i) for i in os.listdir(cla_path)
+                  if os.path.splitext(i)[-1] in supported]
+        # 获取该类别对应的索引
+        image_class = class_indices[cla]
+        # 记录该类别的样本数量
+        every_class_num.append(len(images))
+        # 按比例随机采样验证样本
+        val_path = random.sample(images, k=int(len(images) * val_rate))
+
+        for img_path in images:
+            if img_path in val_path:  # 如果该路径在采样的验证集样本中则存入验证集
+                val_images_path.append(img_path)
+                val_images_label.append(image_class)
+            else:  # 否则存入训练集
+                train_images_path.append(img_path)
+                train_images_label.append(image_class)
+
+    print("{} images were found in the dataset.".format(sum(every_class_num)))
+    print("{} images for training.".format(len(train_images_path)))
+    print("{} images for validation.".format(len(val_images_path)))
+
+    plot_image = False
+    if plot_image:
+        # 绘制每种类别个数柱状图
+        plt.bar(range(len(flower_class)), every_class_num, align='center')
+        # 将横坐标0,1,2,3,4替换为相应的类别名称
+        plt.xticks(range(len(flower_class)), flower_class)
+        # 在柱状图上添加数值标签
+        for i, v in enumerate(every_class_num):
+            plt.text(x=i, y=v + 5, s=str(v), ha='center')
+        # 设置x坐标
+        plt.xlabel('image class')
+        # 设置y坐标
+        plt.ylabel('number of images')
+        # 设置柱状图的标题
+        plt.title('flower class distribution')
+        plt.show()
+
+    return train_images_path, train_images_label, val_images_path, val_images_label
+
+
+def plot_data_loader_image(data_loader):
+    batch_size = data_loader.batch_size
+    plot_num = min(batch_size, 4)
+
+    json_path = './class_indices.json'
+    assert os.path.exists(json_path), json_path + " does not exist."
+    json_file = open(json_path, 'r')
+    class_indices = json.load(json_file)
+
+    for data in data_loader:
+        images, labels = data
+        for i in range(plot_num):
+            # [C, H, W] -> [H, W, C]
+            img = images[i].numpy().transpose(1, 2, 0)
+            # 反Normalize操作
+            img = (img * [0.229, 0.224, 0.225] + [0.485, 0.456, 0.406]) * 255
+            label = labels[i].item()
+            plt.subplot(1, plot_num, i+1)
+            plt.xlabel(class_indices[str(label)])
+            plt.xticks([])  # 去掉x轴的刻度
+            plt.yticks([])  # 去掉y轴的刻度
+            plt.imshow(img.astype('uint8'))
+        plt.show()
+
+
+def write_pickle(list_info: list, file_name: str):
+    with open(file_name, 'wb') as f:
+        pickle.dump(list_info, f)
+
+
+def read_pickle(file_name: str) -> list:
+    with open(file_name, 'rb') as f:
+        info_list = pickle.load(f)
+        return info_list
+
+
+def train_one_epoch(model, optimizer, data_loader, device, epoch):
+    model.train()
+    loss_function = torch.nn.CrossEntropyLoss()
+    accu_loss = torch.zeros(1).to(device)  # 累计损失
+    accu_num = torch.zeros(1).to(device)   # 累计预测正确的样本数
+    optimizer.zero_grad()
+    
+    training_loss = 0.0
+    sample_num = 0
+    length = len(data_loader.dataset)
+    data_loader = tqdm(data_loader, file=sys.stdout)
+    for step, data in enumerate(data_loader):
+        images, labels = data
+        labels1 = torch.zeros(labels.size(0), 2).scatter_(1, labels.view(-1, 1), 1.)
+        sample_num += images.shape[0]
+#         pred = model(images.to(device))
+        labels1_pred , images_recon = model(images.to(device),labels1.to(device))
+        pred_classes = torch.max(labels1_pred, dim=1)[1]
+        accu_num += torch.eq(pred_classes, labels.to(device)).sum()
+        
+#         loss = loss_function(labels1_pred, labels.to(device))
+#         loss.backward()
+#         accu_loss += loss.detach()
+        loss = caps_loss(labels1.to(device), labels1_pred, images.to(device), images_recon, 0.0005 * 784)
+        loss.backward()
+        training_loss += loss.item() * images.to(device).size(0)  # record the batch loss
+        accu_loss += loss.detach()
+
+        data_loader.desc = "[train epoch {}] loss: {:.3f}, acc: {:.3f}".format(epoch,
+                                                                               training_loss / length,
+#                                                                                accu_loss.item() / (step + 1),
+                                                                               accu_num.item() / length)
+
+        if not torch.isfinite(loss):
+            print('WARNING: non-finite loss, ending training ', loss)
+            sys.exit(1)
+
+        optimizer.step()
+        optimizer.zero_grad()
+
+    return training_loss / length, accu_num.item() / length
+
+
+@torch.no_grad()
+def evaluate(model, data_loader, device, epoch):
+    loss_function = torch.nn.CrossEntropyLoss()
+
+    model.eval()
+
+    accu_num = torch.zeros(1).to(device)   # 累计预测正确的样本数
+    accu_loss = torch.zeros(1).to(device)  # 累计损失
+
+    sample_num = 0
+    training_loss = 0.0
+    length = len(data_loader.dataset)
+    data_loader = tqdm(data_loader, file=sys.stdout)
+    for step, data in enumerate(data_loader):
+        images, labels = data  
+        labels1 = torch.zeros(labels.size(0), 2).scatter_(1, labels.view(-1, 1), 1.)
+        sample_num += images.shape[0]
+        
+        labels1_pred , images_recon = model(images.to(device),labels1.to(device))
+        pred_classes = torch.max(labels1_pred, dim=1)[1]
+        accu_num += torch.eq(pred_classes, labels.to(device)).sum()
+        
+
+#         loss = loss_function(labels1_pred, labels.to(device))
+#         accu_loss += loss
+        loss = caps_loss(labels1.to(device), labels1_pred, images.to(device), images_recon, 0.0005 * 784)
+        training_loss += loss.item() * images.to(device).size(0)  # record the batch loss
+
+        data_loader.desc = "[valid epoch {}] loss: {:.3f}, acc: {:.3f}".format(epoch,
+                                                                               training_loss / length,
+#                                                                                accu_loss.item() / (step + 1),
+                                                                               accu_num.item() / length)
+
+    return training_loss / length, accu_num.item() / length
+def plot_log(filename, show=True):
+    # load data
+    keys = []
+    values = []
+    with open(filename, 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            if keys == []:
+                for key, value in row.items():
+                    keys.append(key)
+                    values.append(float(value))
+                continue
+
+            for _, value in row.items():
+                values.append(float(value))
+
+        values = np.reshape(values, newshape=(-1, len(keys)))
+
+    fig = plt.figure(figsize=(4,6))
+    fig.subplots_adjust(top=0.95, bottom=0.05, right=0.95)
+    fig.add_subplot(211)
+    epoch_axis = 0
+    for i, key in enumerate(keys):
+        if key == 'epoch':
+            epoch_axis = i
+            values[:, epoch_axis] += 1
+            break
+    for i, key in enumerate(keys):
+        if key.find('loss') >= 0:  # loss
+            print(values[:, i])
+            plt.plot(values[:, epoch_axis], values[:, i], label=key)
+    plt.legend()
+    plt.title('Training loss')
+
+    fig.add_subplot(212)
+    for i, key in enumerate(keys):
+        if key.find('acc') >= 0:  # acc
+            plt.plot(values[:, epoch_axis], values[:, i], label=key)
+    plt.legend()
+    plt.grid()
+    plt.title('Accuracy')
+
+    # fig.savefig('result/log.png')
+    if show:
+        plt.show()
+
+
+def combine_images(generated_images):
+    num = generated_images.shape[0]
+    width = int(math.sqrt(num))
+    height = int(math.ceil(float(num)/width))
+    shape = generated_images.shape[1:3]
+    image = np.zeros((height*shape[0], width*shape[1]),
+                     dtype=generated_images.dtype)
+    for index, img in enumerate(generated_images):
+        i = int(index/width)
+        j = index % width
+        image[i*shape[0]:(i+1)*shape[0], j*shape[1]:(j+1)*shape[1]] = \
+            img[:, :, 0]
+    return image
+
+
+if __name__=="__main__":
+    plot_log('result/log.csv')
